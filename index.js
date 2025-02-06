a34b7cee8a38e86d73f92b30b1509488a03991e8
Shailesh_18_ <rushikeshkanfade123@gmail.com>
2025-02-06 07:42:04 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.get("/twitter", (req, res) => {
+  res.send("shaileshditcom");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
